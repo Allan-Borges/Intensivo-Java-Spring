3169b3ea36b76110d332f11283993df06a677def
Allan-Borges <alanborges195@gmail.com>
2023-05-11 14:27:34 -0300

    Game repository, service, controller

---
+++ src/main/java/com/alandev/dslist/controllers/GameController.java
@@ -0,0 +1,26 @@
+package com.alandev.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alandev.dslist.dto.GameMinDto;
+import com.alandev.dslist.entities.Game;
+import com.alandev.dslist.services.GameService;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+	@Autowired
+	private GameService gameService;
+
+	@GetMapping
+	public List<GameMinDto> findAll() {
+		List<GameMinDto> result = gameService.findAll();
+		return result;
+	}
+}
